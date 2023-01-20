c9af4b5dd86e2ff924c8793a4e343310e6d01311
aragong <german.aragon@unican.es>
2023-01-20 11:28:04 +0100

    update

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "display_version": True,
+    "style_external_links": False,
 }
 # html_theme = 'furo'
 # html_theme = 'sphinx_book_theme'
