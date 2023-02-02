9514bca0b2b021c6cba33cedcaea784713c1ed31
aragong <german.aragon@unican.es>
2023-02-02 10:27:06 +0100

    fix spelling

--- pyteseo/export/__init__.py
+++ pyteseo/export/__init__.py
@@ -1,3 +1,3 @@
-"""Functions responsible to export results in DataFrames readed with 'pyteseo.io' to standard format files (csv, json, netCDF...)
-There are grouped by type of results (particles, properties, and grids)
+"""Package to export results from DataFrames (loaded with 'pyteseo.io') to standard format files (csv, json, netCDF...)
+There are grouped by type of result: particles, properties, or grids.
 """
