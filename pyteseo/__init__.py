51c4e67e293140a970344572bda2486fb135b9c1
aragong <german.aragon@unican.es>
2023-02-02 10:21:48 +0100

    add url

--- pyteseo/__init__.py
+++ pyteseo/__init__.py
@@ -1,4 +1,4 @@
-"""Python package developed to simplify and facilitate the setup and processing of TESEO simulations (TESEO is a lagrangian numerical model developed by IHCantabria)
+"""Python package developed to simplify and facilitate the setup and processing of TESEO simulations (https://ihcantabria.com/en/specialized-software/teseo/)
 """
 
 __version__ = "0.0.6"
