6a311fb7a3a98d932663b3ee7b56d8d6f811d2b0
aragong <german.aragon@unican.es>
2023-02-02 10:20:01 +0100

    add docstrings

---
+++ pyteseo/export/__init__.py
@@ -0,0 +1,3 @@
+"""Functions responsible to export results in DataFrames readed with 'pyteseo.io' to standard format files (csv, json, netCDF...)
+There are grouped by type of results (particles, properties, and grids)
+"""
---
+++ pyteseo/plot/__init__.py
@@ -0,0 +1,3 @@
+"""Functions responsible to plot TESEO's results.
+Input data for ploting is the pd.DataFrames obtained by means of pyteseo.io subpackage.
+"""
